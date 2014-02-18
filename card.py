2699ac67a529503e1176a4a3279a2d7de569928a
Euler <euler@godputer.(none)>
2014-02-17 22:58:22 -0800

    updated deck with the latest cards. read_cards.py now reads the data from the database_test (for now). I'd give it a B for now. creature card class instance stuff removed from card.py. card.py needs to do a better job of displaying card attributes properties. Give this a D.

--- card.py
+++ card.py
@@ -1,91 +1,113 @@
 import termcolor as tc
-import mana
-import creature
+#import creature
 
 class card:
+   card_name = None
+   Type = None
+   sub_type = None
    complete = False
    permanent = True
    quantity = 0
-   color = "none"
-   card_name = ""
-   card_text = ""
-   flavor_text = ""
-   expansion = ""
-   rarity = "common"
+   color = list()
+   card_text = None
+   flavor_text = None
+   expansion = None
+   rarity = 'C'
    card_number = 0
-   artist = ""
-   watermark = ""
+   artist = None
+   watermark = None
    multiverseid = 0
    tapped = False
    can_untap = True
    can_tap = True
-   converted_mana_cost = 0
-   colorless_mana = 0
-   white_mana = 0
-   green_mana = 0
-   blue_mana = 0
-   red_mana = 0
-   black_mana = 0
+   converted_mana_cost = None
+   colorless = None
+   white_mana = None
+   green_mana = None
+   blue_mana = None
+   red_mana = None
+   black_mana = None
 
-   # Cost
-   cost = mana.Mana()
+########################
+
+   def __init__(self, card_name):
+      self.card_name = card_name
+
+########################
+
+   def setcost(self, colorless, white_mana, blue_mana, black_mana, red_mana, green_mana, converted_mana_cost):
+      if white_mana:
+         self.white_mana = white_mana
+         self.color.append('W')
+      if blue_mana:
+         self.blue_mana = blue_mana
+         self.color.append('U')
+      if black_mana:
+         self.black_mana = black_mana
+         self.color.append('B')
+      if red_mana:
+         self.red_mana = red_mana
+         self.color.append('R')
+      if green_mana:
+         self.green_mana = green_mana
+         self.color.append('G')
+      if colorless:
+         self.colorless = colorless
+   
+########################
+
+   def settype(self, Type, sub_type):
+      if Type: self.Type = Type
+      if sub_type: self.sub_type = sub_type
+
+########################
+      
+   def settext(self, card_text, flavor_text):
+      if card_text: self.card_text = card_text
+      if flavor_text: self.flavor_text = flavor_text
+
+########################
 
    def tap(self):
       if can_tap:
          self.tapped = True
 
+########################
+
    def untap(self):
       if can_untap:
          self.tapped = False
 
+########################
+
    def attack():
       pass
+
+########################
 
    def block():
       pass
 
-############################################
+########################
 
-   def print_card(self, action):
-      match = False
-      action = action.lower()
-      if action == self.card_name.lower():
-         match = True
-         print ''
-         if self.color == "white":
-            print tc.colored(self.card_name,'grey','on_white')
-         elif self.color == "green":
-            print tc.colored(self.card_name,'white','on_green')
-         elif self.color == "blue":
-            print tc.colored(self.card_name,'white','on_blue')
-         elif self.color == "black":
-            print tc.colored(self.card_name,'white','on_grey')
-         elif self.color == "red":
-            print tc.colored(self.card_name,'white','on_red')
-         else:
-            print self.card_name
-
-         print "cost\t",
-         if self.colorless_mana > 0:
-            print "%s" % self.colorless_mana,
-         if self.white_mana > 0:
-            print "%s" % tc.colored(self.white_mana, 'grey','on_white'), 
-         if self.green_mana > 0:
-            print "%s" % tc.colored(self.green_mana, 'white','on_green'),
-         if self.black_mana > 0:
-            print "%s" % tc.colored(self.black_mana, 'white','on_grey'),
-         if self.red_mana > 0:
-            print "%s" % tc.colored(self.red_mana, 'white','on_red'),
-         if self.blue_mana > 0:
-            print "%s" % tc.colored(self.blue_mana, 'white','on_blue'),
-         print ''
-         print ''
-         print self.card_text
-         print ''
-         print self.flavor_text
-         if isinstance(self, creature.Creature):
-            print ''
-            print "%d/%d" % (self.power, self.toughness)
-         print ''
-
-      return match
+   def print_card(self):
+      print self.card_name
+      print "cost\t",
+      if self.colorless:
+         print "%s" % self.colorless,
+      if self.white_mana:
+         print "%s" % tc.colored(self.white_mana, 'grey','on_white'),
+      if self.blue_mana:
+         print "%s" % tc.colored(self.blue_mana, 'white','on_blue'),
+      if self.green_mana:
+         print "%s" % tc.colored(self.green_mana, 'white','on_green'),
+      if self.black_mana:
+         print "%s" % tc.colored(self.black_mana, 'white','on_grey'),
+      if self.red_mana:
+         print "%s" % tc.colored(self.red_mana, 'white','on_red'),
+      print self.card_text
+      print self.flavor_text
+#         if isinstance(self, creature.Creature):
+#            print ''
+#            print "%d/%d" % (self.power, self.toughness)
+      print ''
--- read_cards.py
+++ read_cards.py
@@ -1,59 +1,118 @@
+import card
 import re
 
-p = re.compile('[ \t]*(card_name|type|color|quantity|blue_mana|black_mana|green_mana|white_mana|red_mana|converted_mana_cost|sub_type|card_text|flavor_text|power|toughness|expansion|rarity|card_number|artist|multiverseid)[ \t]*=[ \t]*(.+)', re.IGNORECASE)
-plus = re.compile('(\+)')
-minus = re.compile('(\-)')
+class read_cards:
+   cards = list()
+   #p = re.compile('[ \t]*(type|color|quantity|colorless|blue|black|green|white|red|converted mana cost|sub type|card text|flavor text|power|toughness|expansion|rarity|card number|artist|multiverse id)[ \t]*=[ \t]*(.+)', re.IGNORECASE)
+   p = re.compile("([\W\w]+)=[ \t]*(.+)", re.IGNORECASE)
+   plus = re.compile('(\+)')
+   minus = re.compile('(\-)')
+   ################################
+   def __init__(self, file_name):
+      self.file_name = file_name
 
-f = open('deck','r')
-line = f.readline()
-while line:
-   if plus.findall(line):
-      line = f.readline()
-      while not plus.findall(line) and line:
-         y = p.findall(line)
-         if not y:
+      f = file(file_name,'r')
+      line = f.readline().strip()
+      while line:
+
+         Type                = None
+         color               = None
+         colorless           = None
+         blue_mana           = None
+         black_mana          = None
+         green_mana          = None
+         white_mana          = None
+         red_mana            = None
+         converted_mana      = None
+         sub_type            = None
+         card_text           = None
+         flavor_text         = None
+         power               = None
+         toughness           = None
+         expansion           = None
+         rarity              = None
+         card_number         = None
+         artist              = None
+         multiverseid        = None
+         card_name           = None
+
+         if self.plus.findall(line):
+            line = f.readline().strip()
+            card_name = line
+            while line and line != '+' and line != '-':
+               y = self.p.findall(line)
+               if not y:
+                  line = f.readline().strip()
+                  continue
+               elif y[0][0].strip() == 'type':
+                  Type = y[0][1]
+               elif y[0][0].strip() == 'sub type':
+                  sub_type = y[0][1]
+               elif y[0][0].strip() == 'colorless':
+                  colorless = y[0][1]
+               elif y[0][0].strip() == 'white':
+                  white_mana = y[0][1]
+               elif y[0][0].strip() == 'blue':
+                  blue_mana = y[0][1]
+               elif y[0][0].strip() == 'black':
+                  black_mana = y[0][1]
+               elif y[0][0].strip() == 'red':
+                  red_mana = y[0][1]
+               elif y[0][0].strip() == 'green':
+                  green_mana = y[0][1]
+               elif y[0][0].strip() == 'converted mana cost':
+                  converted_mana = y[0][1]
+               elif y[0][0].strip() == 'card text':
+                  card_text = y[0][1]
+               elif y[0][0].strip() == 'flavor text':
+                  flavor_text = y[0][1]
+               elif y[0][0].strip() == 'power':
+                  power = y[0][1]
+               elif y[0][0].strip() == 'toughness':
+                  toughness = y[0][1]
+               elif y[0][0].strip() == 'expansion':
+                  expansion = y[0][1]
+               elif y[0][0].strip() == 'rarity':
+                  rarity = y[0][1]
+               elif y[0][0].strip() == 'card number':
+                  card_number = y[0][1]
+               elif y[0][0].strip() == 'artist':
+                  artist = y[0][1]
+               elif y[0][0].strip() == 'multiverse id':
+                  multiverseid = y[0][1]
+               line = f.readline().strip()
+         elif self.minus.findall(line):
             break
-         elif y[0][0] == 'card_name':
-            card_name = y[0][1]
-         elif y[0][0] == 'type':
-            Type = y[0][1]
-         elif y[0][0] == 'color':
-            color = y[0][1]
-         elif y[0][0] == 'quantity':
-            quantity = y[0][1]
-         elif y[0][0] == 'blue_mana':
-            blue_mana = y[0][1]
-         elif y[0][0] == 'black_mana':
-            black_mana = y[0][1]
-         elif y[0][0] == 'green_mana':
-            green_mana = y[0][1]
-         elif y[0][0] == 'white_mana':
-            white_mana = y[0][1]
-         elif y[0][0] == 'red_mana':
-            red_mana = y[0][1]
-         elif y[0][0] == 'converted_mana_cost':
-            converted_mana_cost = y[0][1]
-         elif y[0][0] == 'sub_type':
-            sub_type = y[0][1]
-         elif y[0][0] == 'card_text':
-            card_text = y[0][1]
-         elif y[0][0] == 'flavor_text':
-            flavor_text = y[0][1]
-         elif y[0][0] == 'power':
-            power = y[0][1]
-         elif y[0][0] == 'toughness':
-            toughness = y[0][1]
-         elif y[0][0] == 'expansion':
-            expansion = y[0][1]
-         elif y[0][0] == 'rarity':
-            rarity = y[0][1]
-         elif y[0][0] == 'card_number':
-            card_number = y[0][1]
-         elif y[0][0] == 'artist':
-            artist = y[0][1]
-         elif y[0][0] == 'multiverseid':
-            multiverseid = y[0][1]
-         line = f.readline()
-   elif minus.findall(line):
-      break
-   line = f.readline()
+
+#         print Type
+#         print sub_type
+#         print colorless
+#         print white_mana
+#         print blue_mana
+#         print black_mana
+#         print red_mana
+#         print green_mana
+#         print converted_mana
+#         print card_text
+#         print flavor_text
+#         print power
+#         print toughness
+#         print expansion
+#         print rarity
+#         print card_number
+#         print artist
+#         print multiverseid
+#         print '++++++++++++++++++++'
+
+         this_card = card.card(card_name)
+         this_card.setcost(colorless, white_mana, blue_mana,\
+                           black_mana, red_mana, green_mana, converted_mana)
+         this_card.settype(Type, sub_type)
+         this_card.settext(card_text, flavor_text)
+
+         self.cards.append(this_card)
+      f.flush()
+      f.close()
+      
+   def getcards(self):
+      return self.cards
